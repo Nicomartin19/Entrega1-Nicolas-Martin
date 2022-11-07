227ba2a98db10b8c879d95ae6cc28a73b4604dc1
Nicomartin19 <99270160+Nicomartin19@users.noreply.github.com>
2022-11-07 17:37:11 -0300

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,10 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("login/", views.mi_login, name="login"),
-     path("registrar/", views.registrar, name="registrar"),
+    path("registrar/", views.registrar, name="registrar"),
+    path("perfil/", views.perfil, name="perfil"),
+    path("perfil/editar/", views.editar_perfil, name="editar_perfil"),
+    path("perfil/cambiar-contraseña/", views.CambiarContraseña.as_view(), name="cambiar_contraseña"),
     path("logout/", LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,19 @@ from contextlib import redirect_stderr
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
-from accounts.forms import MiFormularioDeCreacion
-
+from accounts.forms import MiFormularioDeCreacion, EditarPerfilFormulario
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.models import ExtensionUsuario
+ 
 def mi_login(request):
     if request.method == "POST":
         formulario = AuthenticationForm(request, data=request.POST)
         if formulario.is_valid():
             usuario = formulario.get_user()
             login(request, usuario)
+            extensionUsuario, es_nuevo = ExtensionUsuario.objects.get_or_create(user=request.user)
             return redirect("index")
         
     else:
@@ -31,3 +36,45 @@ def registrar(request):
     
     
     return render(request, "accounts/registrar.html", {"formulario": formulario})
+
+@login_required
+def perfil(request):
+   
+    return render(request, "accounts/perfil.html", {})
+
+@login_required
+def editar_perfil(request):
+    user = request.user
+   
+    
+    if request.method == "POST":
+       formulario = EditarPerfilFormulario(request.POST, request.FILES)
+       
+       if formulario.is_valid():
+           data_nueva = formulario.cleaned_data
+           user.first_name = data_nueva["first_name"] 
+           user.last_name = data_nueva["last_name"]
+           user.email = data_nueva["email"]
+           user.extensionusuario.avatar = data_nueva["avatar"]
+           
+           user.extensionusuario.save()
+           user.save()
+           
+           return redirect("perfil")
+    else:
+        formulario = EditarPerfilFormulario(
+            initial={
+                "first_name": request.user.first_name , 
+                "last_name": request.user.last_name, 
+                "email": request.user.email,
+                "avatar": request.user.extensionusuario.avatar,
+                    }
+            )
+    
+    return render(request, "accounts/editar_perfil.html", {"formulario": formulario})
+
+class CambiarContraseña(LoginRequiredMixin, PasswordChangeView):
+    template_name = "accounts/cambiar_contraseña.html"
+    success_url = "/accounts/perfil/"
+    
+    
